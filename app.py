9f7e54cf0b5ada98f1457c7c1a743f701d17834a
younextz <35924186+younextz@users.noreply.github.com>
2024-06-11 13:32:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from demo_pyecharts import ST_PY_DEMOS
 
 
 def main():
-    st.title("Streamlit ECharts Demo")
+    st.title("Streamlit ECharts Demozzz")
 
     with st.sidebar:
         st.header("Configuration")
